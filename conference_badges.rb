c32400fa14d8ccebb1e75516f4bd2a8886a13b4f
Peter Hwang <nam-joong.hwang@yale.edu>
2019-05-22 03:55:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,9 +13,11 @@ def batch_badge_creator(array)
 end
 
 def assign_rooms(array)
+  new_array = []
   array.each do |assign|
-    puts "Hello, #{assign}! You'll be assigned to room #{array.index(assign) + 1}!"
+    new_array << "Hello, #{assign}! You'll be assigned to room #{array.index(assign) + 1}!"
   end
+  new_array
 end
 
 def printer
